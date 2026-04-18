6d48f095807a2ce33cae5df63a83c72149296462
jito-16 <juanjosearboledaruiz16@gmail.com>
2026-04-17 22:55:15 -0500

    config: actualizacion de la api conectando en render

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://192.168.1.8:3000/api";
+const API_URL = "https://citafacil-backend.onrender.com/api";
 
 export const api = axios.create({
   baseURL: API_URL,
